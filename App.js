213d64522e8817e01c0a61325153d9dde02c6ec8
Bill Buchanan <bbuchanan@gmail.com>
2018-01-11 17:08:26 -0500

    added a text input and tracked changes through local state

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    placeName: ''
+  }
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Something changed!</Text>
+        <TextInput value={this.state.placeName}
+          placeholder="An awesome place"
+          style={{width: 300, borderColor: "black", borderWidth: 1}}
+          onChangeText={this.placeNameChangedHandler}/>
       </View>
     );
   }
