d4a4129757728ab60479525179afe10ae246ed6d
Bill Buchanan <bbuchanan@gmail.com>
2018-01-17 16:30:47 -0500

    implemented the details page as a new screen that contains navigation

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { Navigation } from 'react-native-navigation'
 import { Provider } from 'react-redux';
 
-import AuthScreen from './src/screens/Auth/Auth'
-import SharePlaceScreen from './src/screens/SharePlace/SharePlace'
-import FindPlaceScreen from './src/screens/FindPlace/FindPlace'
-import configureStore from './src/store/configureStore'
+import AuthScreen from './src/screens/Auth/Auth';
+import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
+import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
+import configureStore from './src/store/configureStore';
 
 const store = configureStore();
 
@@ -13,6 +14,7 @@ const store = configureStore();
 Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen, store, Provider);
 Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen, store, Provider);
+Navigation.registerComponent('awesome-places.PlaceDetailScreen', () => PlaceDetailScreen);
 
 // Start app
 
