2dc50f43e45d6bc107030203c9cd91d754da5bac
Bill Buchanan <bbuchanan@gmail.com>
2018-01-18 08:43:57 -0500

    made the heading text a reusable UI component with default styles

--- src/screens/auth/auth.js
+++ src/screens/auth/auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs'
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput'
+import HeadingText from '../../components/UI/HeadingText/HeadingText';
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -11,7 +12,7 @@ class AuthScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Please Login</Text>
+        <HeadingText>Please Login</HeadingText>
         <Button title="Switch to Login" />
         <View style={styles.inputContainer}>
           <DefaultInput placeholder="Your email address" style={styles.input} />
