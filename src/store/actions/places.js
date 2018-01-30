48088f07b0724248581de7d54e10747689252a8b
Bill Buchanan <bbuchanan@gmail.com>
2018-01-30 18:52:14 -0500

    initial foray into the fetch api. Posted a place to firebase

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,15 +1,24 @@
-import { ADD_PLACE, DELETE_PLACE } from './actionTypes'
+import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
-  return {
-    type: ADD_PLACE,
-    placeName: placeName,
-    location: location,
-    image: image
+  return dispatch => {
+    const placeData = {
+      name: placeName,
+      location
+    };
+    fetch("https://my-project-1485530647486.firebaseio.com/places.json", {
+      method: "POST",
+      body: JSON.stringify(placeData)
+    })
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      });
   };
 };
 
-export const deletePlace = (key) => {
+export const deletePlace = key => {
   return {
     type: DELETE_PLACE,
     placeKey: key
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,6 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import placesReducer from './reducers/places';
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import placesReducer from "./reducers/places";
 
 const rootReducer = combineReducers({
   places: placesReducer
@@ -11,7 +12,7 @@ if (__DEV__) {
   composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 }
 const configureStore = () => {
-  return createStore(rootReducer, composeEnhancers());
+  return createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 };
 
 export default configureStore;
