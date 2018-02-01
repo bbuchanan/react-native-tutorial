1b94791085502e8e2f0ad2ca73755b60d942bdd8
Bill Buchanan <bbuchanan@gmail.com>
2018-02-01 06:07:51 -0500

    removed awesome-places.json

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,10 +2,6 @@ import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    const placeData = {
-      name: placeName,
-      location
-    };
     fetch(
       "https://us-central1-my-project-1485530647486.cloudfunctions.net/storeImage",
       {
@@ -17,22 +13,27 @@ export const addPlace = (placeName, location, image) => {
     )
       .then(res => res.json())
       .then(parsedResp => {
-        console.log(parsedResp);
+        const placeData = {
+          name: placeName,
+          location,
+          image: parsedResp.imageUrl
+        };
+        return fetch(
+          "https://my-project-1485530647486.firebaseio.com/places.json",
+          {
+            method: "POST",
+            body: JSON.stringify(placeData)
+          }
+        )
+          .catch(err => console.log(err))
+          .then(res => res.json())
+          .then(parsedRes => {
+            console.log(parsedRes);
+          });
       })
       .catch(err => {
-        console.log("got here");
         console.log(err);
       });
-
-    // fetch("https://my-project-1485530647486.firebaseio.com/places.json", {
-    //   method: "POST",
-    //   body: JSON.stringify(placeData)
-    // })
-    //   .catch(err => console.log(err))
-    //   .then(res => res.json())
-    //   .then(parsedRes => {
-    //     console.log(parsedRes);
-    //   });
   };
 };
 
