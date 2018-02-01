9fa4e9912bcbcf73bf630e96981eaeeff6f5f1ac
Bill Buchanan <bbuchanan@gmail.com>
2018-02-01 09:21:36 -0500

    beginning of firebase authentication. Created a user account

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,8 +1,34 @@
-import { TRY_AUTH } from './actionTypes';
+import { TRY_AUTH } from "./actionTypes";
 
-export const tryAuth = (authData) => {
-  return {
-    type: TRY_AUTH,
-    authData: authData
-  }
-}
+export const tryAuth = authData => {
+  return dispatch => {
+    dispatch(authSignup(authData));
+  };
+};
+
+export const authSignup = authData => {
+  return dispatch => {
+    fetch(
+      "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAWC9lIu6WENu40Vd1y6IZnNCwnsaqY2YM",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: authData.email,
+          password: authData.password,
+          returnSecureToken: true
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    )
+      .catch(err => {
+        console.log(err);
+        alert("Authentication failed, try again.");
+      })
+      .then(res => res.json())
+      .then(parsedRes => {
+        console.log(parsedRes);
+      });
+  };
+};
