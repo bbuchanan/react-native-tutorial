d06db1a62e5458aa09e6c9b72c2ed2725edd3db7
Bill Buchanan <bbuchanan@gmail.com>
2018-02-07 12:07:18 -0500

    handling the expired token

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -35,7 +35,7 @@ export const tryAuth = (authData, authMode) => {
         if (!parsedRes.idToken) {
           alert("Authentication failed, try again.");
         } else {
-          dispatch(authStoreToken(parsedRes.idToken));
+          dispatch(authStoreToken(parsedRes.idToken, parsedRes.expiresIn));
           startMainTabs();
         }
       });
@@ -46,10 +46,14 @@ export const authSignup = authData => {
   return dispatch => {};
 };
 
-export const authStoreToken = token => {
+export const authStoreToken = (token, expiresIn) => {
   return dispatch => {
     dispatch(authSetToken(token));
+    const now = new Date();
+    const expiryDate = now.getTime() + expiresIn * 1000;
+    console.log(now, new Date(expiryDate));
     AsyncStorage.setItem("ap:auth:token", token);
+    AsyncStorage.setItem("ap:auth:expiryDate", expiryDate.toString());
   };
 };
 
@@ -65,14 +69,25 @@ export const authGetToken = () => {
     const promise = new Promise((resolve, reject) => {
       const token = getState().auth.token;
       if (!token) {
+        let fetchedToken;
         AsyncStorage.getItem("ap:auth:token")
           .then(tokenFromStorage => {
+            fetchedToken = tokenFromStorage;
             if (!tokenFromStorage) {
               reject();
               return;
             }
-            dispatch(authSetToken(tokenFromStorage));
-            resolve(tokenFromStorage);
+            return AsyncStorage.getItem("ap:auth:expiryDate");
+          })
+          .then(expiryDate => {
+            const parsedExpiryDate = new Date(parseInt(expiryDate));
+            const now = new Date();
+            if (parsedExpiryDate > now) {
+              dispatch(authSetToken(fetchedToken));
+              resolve(fetchedToken);
+            } else {
+              reject();
+            }
           })
           .catch(err => reject());
       } else {
@@ -89,7 +104,9 @@ export const authAutoSignIn = () => {
     dispatch(authGetToken())
       .catch(err => console.log(err))
       .then(token => {
-        startMainTabs();
+        if (token) {
+          startMainTabs();
+        }
       });
   };
 };
