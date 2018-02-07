15ccbcc57ee54573a54602ad2c5db117e03f5609
Bill Buchanan <bbuchanan@gmail.com>
2018-02-07 13:19:31 -0500

    handling a refresh token scenario

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -4,13 +4,14 @@ import { TRY_AUTH, AUTH_SET_TOKEN } from "./actionTypes";
 import { uiStartLoading, uiStopLoading } from "./index";
 import startMainTabs from "../../screens/MainTabs/startMainTabs";
 
+const API_KEY = "AIzaSyAWC9lIu6WENu40Vd1y6IZnNCwnsaqY2YM";
+
 export const tryAuth = (authData, authMode) => {
   return dispatch => {
     dispatch(uiStartLoading());
-    const apiKey = "AIzaSyAWC9lIu6WENu40Vd1y6IZnNCwnsaqY2YM";
-    let url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${apiKey}`;
+    let url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${API_KEY}`;
     if (authMode == "signup") {
-      url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${apiKey}`;
+      url = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${API_KEY}`;
     }
 
     fetch(url, {
@@ -35,7 +36,13 @@ export const tryAuth = (authData, authMode) => {
         if (!parsedRes.idToken) {
           alert("Authentication failed, try again.");
         } else {
-          dispatch(authStoreToken(parsedRes.idToken, parsedRes.expiresIn));
+          dispatch(
+            authStoreToken(
+              parsedRes.idToken,
+              parsedRes.expiresIn,
+              parsedRes.refreshToken
+            )
+          );
           startMainTabs();
         }
       });
@@ -46,7 +53,7 @@ export const authSignup = authData => {
   return dispatch => {};
 };
 
-export const authStoreToken = (token, expiresIn) => {
+export const authStoreToken = (token, expiresIn, refreshToken) => {
   return dispatch => {
     dispatch(authSetToken(token));
     const now = new Date();
@@ -54,6 +61,7 @@ export const authStoreToken = (token, expiresIn) => {
     console.log(now, new Date(expiryDate));
     AsyncStorage.setItem("ap:auth:token", token);
     AsyncStorage.setItem("ap:auth:expiryDate", expiryDate.toString());
+    AsyncStorage.setItem("ap:auth:refreshToken", refreshToken);
   };
 };
 
@@ -94,10 +102,45 @@ export const authGetToken = () => {
         resolve(token);
       }
     });
-    promise.catch(err => {
-      dispatch(authClearStorage());
-    });
-    return promise;
+    return promise
+      .catch(err => {
+        return AsyncStorage.getItem("ap:auth:refreshToken")
+          .then(refreshToken => {
+            return fetch(
+              `https://securetoken.googleapis.com/v1/token?key=${API_KEY}`,
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: `grant_type=refresh_token&refresh_token=${refreshToken}`
+              }
+            );
+          })
+          .then(res => res.json())
+          .then(parsedRes => {
+            if (parsedRes.id_token) {
+              console.log("Refresh token worked");
+              dispatch(
+                authStoreToken(
+                  parsedRes.id_token,
+                  parsedRes.expires_in,
+                  parsedRes.refresh_token
+                )
+              );
+              return parsedRes.id_token;
+            } else {
+              dispatch(authClearStorage());
+            }
+          });
+      })
+      .then(token => {
+        if (!token) {
+          throw new Error();
+        } else {
+          return token;
+        }
+      });
   };
 };
 
